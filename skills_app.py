d8a7d223d0a8ae5cf0ffac96d66dc1caeffa40cc
mo3tazJ <en.mo3taz@gmail.com>
2025-11-09 02:02:18 +0300

    Defining Basic Routes

--- skills_app.py
+++ skills_app.py
@@ -1,3 +1,28 @@
-from flask import Flask
+from flask import Flask, render_template
 
 skills_app = Flask(__name__)
+
+
+@skills_app.route("/")
+def homepage():
+    return render_template(
+        "homepage.html",
+        context={"title": "Homepage", "test": "TEST"},
+        pagetitle="Homepage",
+    )
+
+
+@skills_app.route("/about")
+def about():
+    return render_template(
+        "about.html",
+        pagetitle="About",
+    )
+
+
+@skills_app.route("/skills")
+def skills():
+    return render_template(
+        "skills.html",
+        pagetitle="Skills",
+    )
